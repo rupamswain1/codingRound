8c4e01f82cd082104b030ae1154c0adda133c308
rupamswain1 <rupamswain1@gmail.com>
2019-05-15 00:01:17 +0530

    comments added

--- src/main/java/testvagrantCodingRound/wait/DynamicWait.java
+++ src/main/java/testvagrantCodingRound/wait/DynamicWait.java
@@ -9,11 +9,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DynamicWait {
 	
+	//Explicit wait for element to be clickable.
 	public static void elementToBeClickable(WebDriver driver, int timeInSecond, WebElement locator)
 	{
 		driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
 		WebDriverWait wait = new WebDriverWait(driver, timeInSecond);
 		wait.until(ExpectedConditions.elementToBeClickable(locator));
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
 }
