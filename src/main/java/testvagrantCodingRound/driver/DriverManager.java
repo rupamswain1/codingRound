3af6f9726940255f77645520feb35f5ef439485b
rupamswain1 <rupamswain1@gmail.com>
2019-05-14 20:41:17 +0530

    methods for driver operation added

--- src/main/java/testvagrantCodingRound/driver/DriverManager.java
+++ src/main/java/testvagrantCodingRound/driver/DriverManager.java
@@ -1,5 +1,27 @@
 package testvagrantCodingRound.driver;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class DriverManager {
 
+	public WebDriver driver;
+	
+	public  DriverManager()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	public void openUrl(String url)
+	{
+		driver.get(url);
+	}
+	public void quit()
+	{
+		driver.quit();
+	}
 }
