3714ae5a91d72fe66f288d6e07eaca1b0f26a455
rupamswain1 <rupamswain1@gmail.com>
2019-05-14 23:58:29 +0530

    Comments added for driverManager

--- src/main/java/testvagrantCodingRound/driver/DriverManager.java
+++ src/main/java/testvagrantCodingRound/driver/DriverManager.java
@@ -11,18 +11,23 @@ public class DriverManager {
 
 	public WebDriver driver;
 	
+	//Initializes chrome driver, can be modified in future to add fire fox, IE and Edge browser
 	public  DriverManager()
 	{
 		WebDriverManager.chromedriver().setup();
 		driver=new ChromeDriver();
 		driver.manage().window().maximize();
+		//implicit wait is made 0 where explicit wait is used
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	
+	//method to open url
 	public void openUrl(String url)
 	{
 		driver.get(url);
 	}
+	
+	//method to close browser
 	public void quit()
 	{
 		driver.quit();
